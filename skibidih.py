7f68ac85b9332363b9fddbb7cceaac8e797c9a26
lyslys2x <alysayong@gmail.com>
2025-09-11 18:31:00 +0800

    final base version

--- skibidih.py
+++ skibidih.py
@@ -10,6 +10,13 @@ class MyCilent(discord.Client):
     async def on_ready(self):
         # Gets the information of the user that logs in
         print("Logged on as {0}".format(self.user))
+    
+    async def on_message(self, message):
+        # Check if the bot is sending a message, if it is sending a message return null
+        if message.on_author == self.user:
+            return 
+        if message.content.startswith("$haii"):
+            await message.channel.senf("Hello :3")
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -17,3 +24,4 @@ intents.message_content = True
 #Instantiate Client Class and 
 discordClient = MyCilent(intents=intents)
 discordClient.run(DISCORD_TOKEN)
+
